5da8c9302a2073f91ab000130f27896f5d1771d6
MichaelLuo520 <po2319987@gmail.com>
2022-01-14 05:39:06 +0800

    import google icon

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+import 'material-icons/iconfont/material-icons.css';
 import {
     createApp
-} from 'vue'
-import App from './App.vue'
-import './assets/css/scss/main.css'
-import router from './router'
+} from 'vue';
+import App from './App.vue';
+import './assets/css/scss/main.css';
+import router from './router';
+
 
 createApp(App).use(router).mount('#app')
