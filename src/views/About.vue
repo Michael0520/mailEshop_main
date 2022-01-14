ba998054a3fcd57d3ce15286faadaea977fe173e
MichaelLuo520 <po2319987@gmail.com>
2022-01-14 15:59:59 +0800

    update card style and clean asset/image

--- src/views/About.vue
+++ src/views/About.vue
@@ -59,6 +59,91 @@
       </div>
     </div>
   </main>
+
+  <section>
+    <div class="about">
+      <div class="about-title">
+        <h2>
+          什麼是
+          <span class="text-pink">賣Eshop</span>
+          ?
+        </h2>
+      </div>
+      <div class="about-content">
+        <p>
+          你只需要
+          <span class="text-pink">開團銷售賺分潤</span>
+          <br />後續訂單、出貨、售後等服務都由 賣Eshop 幫您處理
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="card">
+      <div class="card-top">
+        <div class="card-top-image">
+          <img src="../assets/mainBanner01.png" alt="卡片圖片" />
+        </div>
+        <div class="card-top-title">
+          <h3>如何找到優質、熱心的商品？</h3>
+        </div>
+      </div>
+      <div class="card-bottom">
+        <div class="card-bottom-content">
+          <div class="card-bottom-title">
+            <p>
+              賣Eshop 嚴格把關上百件商品，只為提供您找到最好賣的時
+              <br />下夯品！
+            </p>
+          </div>
+          <div class="card-bottom-listTitle">
+            <div class="card-bottom-listTitle-content">
+              <p>擁有熱銷團購商品列表</p>
+              <span class="material-icons-outlined">task_alt</span>
+            </div>
+            <div class="card-bottom-listTitle-content">
+              <p>各產業優質貨源：食品、家電、生活、旅遊、美妝</p>
+              <span class="material-icons-outlined">task_alt</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-bottom-image">
+          <img src="../assets/mainBanner01.png" alt="卡片圖片" />
+          <div class="card-bottom-image-title">
+            <p class="text-pink">
+              賣Eshop
+              <br />小助理
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-top">
+        <div class="card-top-image">
+          <img src="../assets/mainBanner01.png" alt="卡片圖片" />
+        </div>
+        <div class="card-top-title">
+          <h3>如何找到優質、熱心的商品？</h3>
+        </div>
+      </div>
+      <div class="card-bottom">
+        <div class="card-bottom-title">
+          <p>
+            賣Eshop 嚴格把關上百件商品，只為提供您找到最好賣的時
+            <br />下夯品！
+          </p>
+        </div>
+        <div class="card-bottom-image">
+          <div class="card-top-image">
+            <img src="../assets/mainBanner01.png" alt="卡片圖片" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 
@@ -107,7 +192,6 @@ header {
 main {
   display: flex;
   justify-content: center;
-  align-items: center;
   background-color: #fb8609;
   height: 630px;
   .content {
@@ -115,13 +199,12 @@ main {
     max-width: 1480px;
     justify-content: center;
     align-items: center;
-    // padding: 120px 0px 150px 0px;
+    position: relative;
 
     .left-content {
       display: flex;
       flex-direction: column;
       text-align: left;
-      // margin: 0px 0px 150px 66px;
       color: #fff;
       .left-title {
         h1 {
@@ -146,8 +229,6 @@ main {
         border-radius: 90px;
         border-radius: 90px;
         background: #ffffff;
-        box-shadow: inset -50px 50px 100px #bfbfbf,
-          inset 50px -50px 100px #ffffff;
       }
     }
     .right-content {
@@ -163,16 +244,163 @@ main {
         }
       }
       .right-box {
-        width: 326px;
-        height: 326px;
-        background-color: #fff;
-        color: #312e5f;
         display: flex;
         justify-content: center;
         align-items: center;
+        width: 326px;
+        height: 326px;
+        color: #312e5f;
         font-size: 54px;
+        line-height: 70px;
+        font-weight: bold;
+        background-color: #fff;
+      }
+    }
+    .content-btn {
+      position: absolute;
+      bottom: 30px;
+      button {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 285px;
+        height: 105px;
+        color: #fff;
+        font-size: 32px;
+        border: 0;
+        border-radius: 30px;
+        background: #fc0a52;
+        box-shadow: inset -50px 50px 100px #b00739,
+          inset 50px -50px 100px #ff0d6b;
+        span {
+          font-size: 32px;
+        }
       }
     }
   }
+}
+
+.about {
+  margin-top: 105px;
+  font-weight: bold;
+  .about-title {
+    font-size: 46px;
+    margin-bottom: 32px;
+  }
+  .about-content {
+    font-size: 30px;
+    line-height: 44px;
+  }
+}
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 130px;
+  font-size: 38px;
+  font-weight: 500;
+  .card-top {
+    display: flex;
+    margin-bottom: 160px;
+    .card-top-image {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      bottom: 33px;
+      left: 20px;
+      img {
+        width: 215px;
+        height: 250px;
+        background-size: cover;
+      }
+    }
+    .card-top-title {
+      display: flex;
+      padding: 40px 50px;
+      width: 936px;
+      height: 110px;
+      align-items: center;
+      background-color: #f1f3f4;
+      border-radius: 30px;
+      h3 {
+        text-align: left;
+      }
+    }
+  }
+  .card-bottom {
+    display: flex;
+    margin-bottom: 160px;
+    .card-bottom-content {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      width: 940px;
+      height: 295px;
+      background-color: #ef4a30;
+      padding: 38px 65px 18px 29px;
+      color: #fff;
+      text-align: left;
+      font-size: 32px;
+      border-radius: 30px;
+      .card-bottom-title {
+        margin: 20px;
+      }
+      .card-bottom-listTitle {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        width: 905px;
+        height: 145px;
+        // margin-top: 20px;
+        margin: 15px;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 30px;
+        color: #000;
+        .card-bottom-listTitle-content {
+          display: flex;
+          flex-direction: row-reverse;
+          justify-content: start;
+          p {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          span {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 40px;
+            color: #fc0a52;
+            margin-right: 20px;
+          }
+        }
+      }
+    }
+    .card-bottom-image {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      position: relative;
+      bottom: 33px;
+      right: 20px;
+      img {
+        width: 215px;
+        height: 250px;
+        background-size: cover;
+      }
+      .card-bottom-image-title {
+        font-size: 32px;
+        line-height: 39px;
+      }
+    }
+  }
+}
+
+.text-pink {
+  color: #fc0a52;
 }
 </style>
